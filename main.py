fce0736538ec1c0cc89b56011796f0290f78ccc2
gangi reddy <nallagondu@hotmail.com>
2022-08-22 16:07:54 +0000

    updated

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello ,World ! ")
+print("Hello this is my name:" + input("Name here"))
+print("Hello this is my Second name:" +  input("Second Name here"))
+x = int(input("x value :"))
+y = x *2
+z = y * 20
+print(z)
