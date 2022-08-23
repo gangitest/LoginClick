cb601349725cda03319eae9f2bf54d1dffaecc7b
gangi reddy <nallagondu@hotmail.com>
2022-08-23 04:14:32 +0000

    updated with original code

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-print("Hello ,World ! ")
-print("Hello this is my name:" + input("Name here"))
-print("Hello this is my Second name:" +  input("Second Name here"))
-x = int(input("x value :"))
-y = x *2
-z = y * 20
-print(z)
+# Author:Gangireddy
+
+from selenium import webdriver
+
+def get_drvier():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(options=options)
+  driver.get("https://en.wikipedia.org/wiki/Main_Page")
+  return driver
+
+def main():
+  driver = get_drvier()
+  element = driver.find_element(by="xpath", value='//*[@id="mp-tfp"]/table')
+  return element.text
+
+print(main())
+
